f007b8aadbca980f5ca9b4de310a82be0acc516b
LebedevaAlena <loik-alena@mail.ru>
2016-11-30 03:34:02 +0200

    Added frien void and void Result1

--- Task.cpp
+++ Task.cpp
@@ -20,6 +20,11 @@ Task::Task(double x, double y, double z)
 
 void Task::Result1()
 {
-    c = ((pow(x, y + 1) + exp( y -1)) / (1 + x * fabs(y - tan(z)))) * (1 + fabs(y - x))
-    + ((fabs(y - x) * fabs(y -x)) / 2) - (pow(fabs(y - x), 3) / 3);
+    c = ((1 + fabs(y - x))*(pow(x, y + 1) + exp(y -1))) / (1 + x * fabs(y - tan(z)))
+    + (pow(fabs(y -x), 2)) / 2 - (pow(fabs(y - x), 3)) / 3;
+}
+
+void printRes(Task &res)
+{
+     cout << "c=" << res.c << '\t' << "z=" << res.z;
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include <math.h>
+#include "Task.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Task a(2.444, 0.869 * pow(10, -2), -0.13 * pow(10, 3));
+    a.Result1();
+    printRes(a);
     return 0;
 }
